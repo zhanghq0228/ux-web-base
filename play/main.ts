e0ba983ced4ad44d9a6cf0ae0e6ac8ee8bc1bc6b
张宏强 <hongqiang.zhang@ux168.cn>
2023-09-07 17:26:54 +0800

    test

--- play/main.ts
+++ play/main.ts
@@ -3,4 +3,5 @@ import App from './app.vue';
 import UxWebBase from '@ux-web-base/components';
 const app = createApp(App);
 app.use(UxWebBase);
+console.log(`output->test`, 'test');
 app.mount('#app');
